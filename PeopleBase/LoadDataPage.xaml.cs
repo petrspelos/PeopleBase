840b154bfb5b0dcc1e56b644af0a29a6e63afffb
petrspelos <petr.spelos@gmail.com>
2019-04-07 17:15:39 +0200

    Add project files.

---
+++ PeopleBase/LoadDataPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PeopleBase
+{
+    /// <summary>
+    /// Interaction logic for LoadDataPage.xaml
+    /// </summary>
+    public partial class LoadDataPage : Page
+    {
+        public LoadDataPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFromFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PeopleListingPage());
+        }
+    }
+}
